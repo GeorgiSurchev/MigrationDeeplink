890671549f379fae4f5a229ca4df6a88b586b763
Georgi Sruchev <georgi.surchev@scalefocus.com>
2022-04-13 13:04:37 +0300

    -add dataBinding,firebase-dynamic-link dependency -create DynamicLinkDataHelper.kt -add implementation for Dynamic Link creation in MainFragment.kt

---
+++ app/src/main/java/com/example/migrationdeeplink/DynamicLinkDataHelper.kt
@@ -0,0 +1,46 @@
+package com.example.migrationdeeplink
+
+import android.net.Uri
+import android.util.Base64
+import android.util.Log
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.dynamiclinks.ktx.shortLinkAsync
+import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
+
+object DynamicLinkDataHelper {
+
+	private const val MIGRATION_DYNAMIC_LINK_PREFIX = "https://migrationdeeplink.page.link/?link="
+	private const val MIGRATION_DATA_KEY = "?migrationData="
+	private const val MIGRATION_DYNAMIC_LINK_SUFFIX = "&apn=com.example.migrationdeeplink"
+	private const val MIGRATION_DEEPLINK_FALLBACK_URL = "https://www.google.com"
+
+	var migrationDynamicLink = ""
+
+	fun setMigrationDynamicLinkData(migrationData: MigrationData) {
+		val jsonString = Gson().toJson(migrationData)
+		val encodedMigrationData = String(Base64.encode(jsonString.toByteArray(), Base64.NO_WRAP), Charsets.UTF_8)
+		val migrationLink = createMigrationLink(encodedMigrationData)
+		Firebase.dynamicLinks.shortLinkAsync {
+			longLink = Uri.parse(migrationLink)
+		}.addOnSuccessListener {
+			migrationDynamicLink = it.shortLink.toString()
+		}.addOnFailureListener {
+			Log.d("migrationLinkFailed", it.toString())
+		}
+	}
+
+	private fun createMigrationLink(encodedMigrationData: String) = StringBuilder().append(
+		MIGRATION_DYNAMIC_LINK_PREFIX
+	).append(
+		MIGRATION_DEEPLINK_FALLBACK_URL
+	).append(
+		MIGRATION_DATA_KEY
+	).append(
+		encodedMigrationData
+	).append(
+		MIGRATION_DYNAMIC_LINK_SUFFIX
+	).toString()
+
+	data class MigrationData(val name: String, val sureName: String, val email: String, val phone: String)
+}
--- app/src/main/java/com/example/migrationdeeplink/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/migrationdeeplink/ui/main/MainFragment.kt
@@ -1,19 +1,23 @@
 package com.example.migrationdeeplink.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.migrationdeeplink.DynamicLinkDataHelper
+import com.example.migrationdeeplink.DynamicLinkDataHelper.migrationDynamicLink
+import com.example.migrationdeeplink.DynamicLinkDataHelper.setMigrationDynamicLinkData
 import com.example.migrationdeeplink.R
+import com.example.migrationdeeplink.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
-	companion object {
-
-		fun newInstance() = MainFragment()
-	}
+	private lateinit var binding: MainFragmentBinding
 
 	private lateinit var viewModel: MainViewModel
 
@@ -21,12 +25,22 @@ class MainFragment : Fragment() {
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View {
-		return inflater.inflate(R.layout.main_fragment, container, false)
+		binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+		return binding.root
 	}
 
 	override fun onActivityCreated(savedInstanceState: Bundle?) {
 		super.onActivityCreated(savedInstanceState)
-		viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-		// TODO: Use the ViewModel
+		viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+		binding.migrationDeeplinkBtn.setOnClickListener {
+			val name = binding.nameEt.text.toString()
+			val sureName = binding.surnameEt.text.toString()
+			val email = binding.emailEt.text.toString()
+			val phone = binding.phoneEt.text.toString()
+			val migrationData = DynamicLinkDataHelper.MigrationData(name, sureName, email, phone)
+			setMigrationDynamicLinkData(migrationData)
+			if (migrationDynamicLink.isEmpty()) return@setOnClickListener
+			startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(migrationDynamicLink)))
+		}
 	}
 }
